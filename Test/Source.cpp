62a0d99cf61b60ad1a01741ce4dd1898d1a214b3
Untermensch <70441070+AbsShtrudell@users.noreply.github.com>
2022-09-07 15:04:17 +0300

    tutorial

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,3 +1,9 @@
 #include <windows.h>
 #include <d3d11.h>
 #include <d3dx11.h>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	while (true);
+	return 0;
+}
